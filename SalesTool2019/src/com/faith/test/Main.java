208a998de1a0d4c5d99b4d9599c67c9f866a62d0
hp <hp@DESKTOP-V438SGR>
2019-10-04 10:23:32 +0530

    initial commit

---
+++ SalesTool2019/src/com/faith/test/Main.java
@@ -0,0 +1,16 @@
+package com.faith.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData objSalesData= new SalesData();
+		System.out.println("Hello UST guys! Git Repo");
+		displayGreettings();
+		objSalesData.display();
+		}
+	private static void displayGreettings(){
+		System.out.println("Welcome to sales Report App 2019");
+	}
+
+}
